60f00c96b57bbe0771a0f304b2d5c8fa6d3786af
Otavius Whitehead <developer.otavius@gmail.com>
2025-12-16 19:43:51 -0500

    candle timing class

--- api_test.py
+++ api_test.py
@@ -1,5 +1,6 @@
 from api.oanda_api import OandaApi
 from infrastructure.instrument_collection import instrumentCollection
+from models.candle_timing import CandleTiming
 from simulation.ma_cross import run_ma_sim
 from dateutil import parser
 from infrastructure.collect_data import run_collection
@@ -11,9 +12,5 @@ if __name__ == "__main__":
     oanda_api = OandaApi()
 
     instrumentCollection.load_instruments("./data")
-    #trade_id = oanda_api.place_trade("EUR_USD", 100, 1)
-    #print("Opened", trade_id)
-    #time.sleep(10)
-    print(f"Closing {76}", oanda_api.close_trade(76))
-    time.sleep(8)
-    [print(x) for x in oanda_api.get_open_trades()]
+    dd = oanda_api.last_complete_candle("EUR_USD", granularity="M5")
+    print(CandleTiming(dd))
