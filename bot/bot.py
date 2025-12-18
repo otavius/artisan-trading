31601931867164dbaf435b105c55f910cdec5ebf
Otavius Whitehead <developer.otavius@gmail.com>
2025-12-17 20:15:41 -0500

    added updated timings function to the candle manager class

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from bot.candle_manager import CandleManager
 from infrastructure.log_wrapper import LogWrapper
@@ -10,7 +11,8 @@ class Bot:
 
     ERROR_LOG = "error"
     MAIN_LOG = "main"
-    GRANULARITY = "M5"
+    GRANULARITY = "M1"
+    SLEEP = 10
 
     def __init__(self):
         self.load_settings()
@@ -43,3 +45,15 @@ class Bot:
 
     def log_to_error(self, msg):
         self.log_message(msg, Bot.ERROR_LOG)
+
+    def process_candles(self, triggered):
+        if len(triggered) > 0:
+            self.log_message(f"process_candles triggered: {triggered}", Bot.MAIN_LOG)
+            for p in triggered:
+                pass
+
+
+    def run(self):
+        while True:
+            time.sleep(Bot.SLEEP)
+            self.process_candles(self.candle_manager.update_timings())
--- run_bot.py
+++ run_bot.py
@@ -2,3 +2,4 @@ from bot.bot import Bot
 
 if __name__ == "__main__":
     bot = Bot()
+    bot.run()
